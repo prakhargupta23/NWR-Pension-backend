8adc11cc5c706da42223b241ad4e248d84edce84
Prakhar Gupta <136190337+prakhargupta23@users.noreply.github.com>
2026-02-09 17:24:35 +0530

    Removed autentication for chatbot

--- get-final-result/index.ts
+++ get-final-result/index.ts
@@ -6,28 +6,28 @@ const httpTrigger: AzureFunction = async function (
   req: HttpRequest
 ): Promise<void> {
   try {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1]; // Splits "Bearer TOKEN"
+    // const authHeader = req.headers["authorization"];
+    // const token = authHeader && authHeader.split(" ")[1]; // Splits "Bearer TOKEN"
 
-    if (!token) {
-      context.res = {
-        status: 401,
-        body: "No token provided",
-      };
-      return;
-    }
+    // if (!token) {
+    //   context.res = {
+    //     status: 401,
+    //     body: "No token provided",
+    //   };
+    //   return;
+    // }
 
     let decoded = {};
 
-    try {
-      decoded = jwt.verify(token, process.env.secret);
-    } catch (error) {
-      context.res = {
-        status: 401,
-        body: { error: "Invalid or expired token" },
-      };
-      return;
-    }
+    // try {
+    //   decoded = jwt.verify(token, process.env.secret);
+    // } catch (error) {
+    //   context.res = {
+    //     status: 401,
+    //     body: { error: "Invalid or expired token" },
+    //   };
+    //   return;
+    // }
     // Call the insertData function
     const result = await processUserQuery(
       req.body.query,
