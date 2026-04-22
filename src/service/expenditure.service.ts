15b7650e407557131ed5fb1e393522b4bf68f8c0
Prakhar Gupta <136190337+prakhargupta23@users.noreply.github.com>
2026-04-22 17:44:55 +0530

    Update OCR service endpoint URL

--- src/service/expenditure.service.ts
+++ src/service/expenditure.service.ts
@@ -51,7 +51,7 @@ export async function getfiledata(prompt: string, file: string) {
       // const response = await axios.post('https://ocrappnwrsup-bwhhbsenaeb8gqdm.canadacentral-01.azurewebsites.net/ocr', {
       //   pdfBase64: file
       // });
-      const response = await axios.post('https://pdfocrazure.azurewebsites.net/ocr', {
+      const response = await axios.post('https://ocrapp-g0b5ddcsd5e2gsau.centralindia-01.azurewebsites.net/ocr', {
         pdf: file
       });
       extractedText = response.data.text;
